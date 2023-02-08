cef1a7ecb0e6704429d31f5a06c956831636431d
John Shaffer <john@insilica.co>
2023-02-08 12:46:11 -0600

    Add .editorconfig, .gitignore, and nix files
    The awscli dependency will be used for syncing to S3

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "srvc-recogito";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell { buildInputs = [ awscli ]; };
+      });
+}
